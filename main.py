f366acdee3f2910786705d5d6ecf0b3cebb2a9a7
Daveed <daveed925@gmail.com>
2022-09-08 16:43:51 +0100

    Created a list to reduce code

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import random
 
+options = ["rock", "paper", "scissors"]
+
 while True:
     user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
     if user_input == "q":
         break
 
-    if user_input not in ["rock", "paper", "scissors"]:
+    if user_input not in options:
         print("Type something valid")
         continue
 
